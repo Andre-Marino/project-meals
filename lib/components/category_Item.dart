e9f8c2a066b4eab0ed32fbabf651bb709d97c3be
Andre Marino <andretorresmarino@gmail.com>
2024-03-05 14:16:40 -0300

    criando estrutura

---
+++ lib/components/category_Item.dart
@@ -0,0 +1,22 @@
+import '../models/category.dart';
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryItem(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(category.title),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [
+          category.color.withOpacity(0.5),
+          category.color,
+        ],
+      )),
+    );
+  }
+}
